aa30b2bc46e6eb7d1a47d95f2db8f2d506732698
Robeil Aregawi <robeilaregawi1.com>
2024-07-29 09:42:01 -0700

    added work experience

--- src/components/about/Experience.jsx
+++ src/components/about/Experience.jsx
@@ -2,6 +2,30 @@ import React from "react";
 
 const experienceContent = [
   {
+    year: "Jun 2023 - Present",
+    position: "Full Stack Engineer",
+    companyName: "Blankfactor LLC",
+    location: "Remote",
+    details: `The platform is a unique opportunity for teachers to offer their services while students learn, all while 
+    contributing to a good cause. Income earned from the platform will be donated to Eritrean immigrants worldwide. 
+    It's a win-win situation, as students get to learn and improve their skills while helping a good cause.`,
+    responsibilities: [
+      `Participate and contribute to architectural meetings.`,
+      `Developed and deployed Spring batch processing application to handle large number of records.`,
+      `Extensive experience in writing test cases using JUnit 5 and Mockito.`,
+      `Developed scripts to handle user data conversion.`,
+      `Developed application using spring Boot using Spring web, Spring Data, Spring Security.`,
+      `Worked in developing and deploying RESTful microservices on the AWS Cloud using Spring Boot and
+      Spring Cloud, utilizing various components.`,
+      `Update and manage config server to centrally control app configs for each application in pre-prod and prod.`,
+      `Maintained software and documented programming tasks and procedures. `,
+      `Implemented mapping of associations between entity classes and the relationship between logic.`,
+      `Utilize dependency injection and constructor injection when creating components.`,
+      `Wrote clean, efficient, and maintainable code that follows industry best practice and coding standard..`
+    ],
+    technologies: `Java, Spring, Spring Boot, REST, SQL, JUnit, Mockito, IntelliJ, Maven, Microservices, HTML, REACT, CSS, JavaScript, GIT`
+  },
+  {
     year: "Jan 2023 - Jul  2023",
     position: "Java Developer",
     companyName: "Kabbee Organization",
--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+//import awsExports from "./aws-exports";
+//Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
