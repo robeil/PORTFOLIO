a564a290ca24d6a59b8710b8c3e5b66174301926
Robeil Aregawi <robeilaregawi1.com>
2025-08-18 23:24:29 -0700

    clean up

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import "./assets/scss/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { Amplify } from "aws-amplify";
-//import awsExports from "./aws-exports";
-//Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -18,7 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
